a52e13076130ed41de026fcb6cfabe5e8ccfa41b
k <k@klabz.org>
2018-09-25 12:53:53 +0200

    0.1.2 - Link: properly accept more props with new react-next meta

--- src/react/router/Link.hx
+++ src/react/router/Link.hx
@@ -21,20 +21,13 @@ typedef LinkProps = {
 		Allows access to the underlying ref of the component.
 	*/
 	@:optional var innerRef:HtmlElement->Void;
-
-	// Note: all props will also be passed to the <a> element via spread,
-	// so className, title, id and such are accepted too.
-	@:optional var className:String;
-	@:optional var title:String;
-	@:optional var id:String;
-	@:optional var target:String;
-	@:optional var rel:String;
 }
 
 /**
 	Provides declarative, accessible navigation around your application.
 	See https://reacttraining.com/react-router/web/api/Link
 */
+@:acceptsMoreProps
 @:jsRequire('react-router-dom', 'Link')
 extern class Link extends ReactComponentOfProps<LinkProps> {}
 
